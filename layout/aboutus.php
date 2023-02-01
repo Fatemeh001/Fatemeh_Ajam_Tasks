a47876572753f0f2923d0ec9293503cfbcc844a8
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-02-01 12:27:08 +0200

    a

---
+++ layout/aboutus.php
@@ -0,0 +1,7 @@
+
+<?php include "header.php" ?>
+<h2> About Us </h2>
+<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam odit dignissimos placeat fuga alias exercitationem ut! Nostrum perferendis, rerum ratione ea et architecto blanditiis voluptatem ducimus reprehenderit sit odit.</p>
+
+
+<?php include "footer.php" ?>
---
+++ layout/index.php
@@ -0,0 +1,16 @@
+<?php include "header.php" ?>
+<h1> some content <h1>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p> Odit aperiam eveniet maiores suscipit ea cumque soluta quisquam modi, </p>
+    <p>nesciunt sint, blanditiis recusandae commodi </p>
+    <p>quos ad eligendi atque consectetur, aliquid vitae?</p>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p> Odit aperiam eveniet maiores suscipit ea cumque soluta quisquam modi, </p>
+    <p>nesciunt sint, blanditiis recusandae commodi </p>
+    <p>quos ad eligendi atque consectetur, aliquid vitae?</p>
+
+    <?php include "footer.php" ?>
