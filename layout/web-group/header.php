1f0de45cfa04c364f117cb7a80bb4eaefabe1609
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-02-02 19:18:24 +0200

    a

---
+++ layout/web-group/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Cecile | Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php echo $title ?></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+    <header>
+        <div class="container">
+          <div class="logo">
+            <a href="index.html"><img src="images/logo.png" width="400" heigth="auto" alt="Cecile"></a>
+          </div>
+  
+        
+  
+          <ul class="horizontal-list">
+            <li><a id="Tops" href="tops.html">Tops</a></li>
+            <li><a id="Sale" href="sale.html">Sale</a></li>
+          </ul>
+        </div>
+
