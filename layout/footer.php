9f6fe276a88a4b6753aa363444526b8badd25339
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-02-07 11:35:13 +0200

    ex3

--- layout/footer.php
+++ layout/footer.php
@@ -4,7 +4,7 @@
 <footer style="background-color: rgb(65, 7, 109); color: white;">
 
   <!--  Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Copyright
+  <div class="footer-copyright text-center py-3" >© 2020 Copyright
   </div>
   <!-- Copyright -->
 
