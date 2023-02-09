e14cc7b819bffc478e3e88c0db38cd92c09c5044
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-02-09 23:05:07 +0200

    just comment

--- layout/array.php
+++ layout/array.php
@@ -71,34 +71,46 @@ for ($y = 0; $y < count($arraykey); $y++) {
 ?>
 
 
-<h3>PHP script to calculate and display average temperature, five lowest and highest temperatures.</h3> 
+<h3>6. PHP script to calculate and display average temperature, five lowest and highest temperatures.</h3> 
 
 <?php
-$month_temp = " 78, 60, 62, 68, 71, 68, 73,85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 
-74, 62, 62, 65, 64, 68, 73, 75, 79, 73";
+$month_temp = "78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 81, 76, 73,
+68, 72, 73, 75, 65, 74, 63, 67, 65, 64, 68, 73, 75, 79, 73";
+// what is explode and what does the below code do? : 
+//The explode function receives a string and divides it into different parts based on a delimiter and places it inside the array cells.
+//The explode() function breaks the text string and places each element in a cell of the array
 $temp_array = explode(',', $month_temp);
 $tot_temp = 0;
+// What is count?
+//count() function is an in-built function available in PHP, which counts and returns the number of elements in an array. 
+//It also counts the number of properties in an object.
 $temp_array_length = count($temp_array);
 foreach($temp_array as $temp)
 {
  $tot_temp += $temp;
-}   
+}
  $avg_high_temp = $tot_temp/$temp_array_length;
- echo "Average Temperature is : ".$avg_high_temp."<br>";
-
+ echo "Average Temperature is : ".$avg_high_temp."
+"; 
+// what does sort do?
+//The sort() function sorts an indexed array in ascending order.
 sort($temp_array);
-echo " List of five lowest temperatures :";
+echo "<br> List of five lowest temperatures :";
 for ($i=0; $i< 5; $i++)
 { 
 echo $temp_array[$i].", ";
 }
-echo "<br>";
-echo "List of five highest temperatures :";
+echo "<br>List of five highest temperatures :";
+// explain the following loop
+//Considering that the $temp_array_length function previously sorted the temperature in ascending order, 
+//this loop takes the last 5 and shows that they are five highest temperatures.
+//For example, if we have 50 temperatureS , it will add 46 first and then one by one it adds to it.
 for ($i=($temp_array_length-5); $i< ($temp_array_length); $i++)
 {
 echo $temp_array[$i].", ";
 }
 ?>
+
 
 
 <?php include "footer.php" ?>
