1a44f3f3527d64045a5db985a7d17d28b07f9aa7
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-02-10 14:17:46 +0200

    change something

--- layout/tops.php
+++ layout/tops.php
@@ -24,10 +24,16 @@
             <li><a id="Tops" href="tops.php">Tops</a></li>
             <li><a id="Sale" href="sale.php">Sale</a></li>
             <li class="nav-item">
-          <a class="nav-link" href="ex1.php">Exercise 1</a>
+          <a class="nav-link" href="ex1.php">Ex1</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="variable.php">Exercise 3</a>
+          <a class="nav-link" href="variable.php">Ex3</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="controlflow.php">Ex4</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="array.php">Ex5</a>
         </li>
           </ul>
         </div>
